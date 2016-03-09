ad14461ea6ed04b0031324e26f17a224e30dd757
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-09 09:51:46 -0500

    Update xdo.py

--- KVM/xdo.py
+++ KVM/xdo.py
@@ -2,5 +2,10 @@ http://thenumberzero.blogspot.com/2014/07/a-no-install-solution-to.html
 
 sudo apt-get install xdotool
 
-# windowsize
+# Maximize KVM
 man xdotool
+
+Set "windowsize" to fullscreen size
+
+Use "windowposition" for starting position.
+Then use "mouse click and drag" into negative coordinates!
