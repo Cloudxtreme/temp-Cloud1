2393ef3852af973c824a03218c916a40b1c1624a
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-10 05:55:10 -0500

    Rename Xen/instal.groovy to IaaS/xen/new.groovy

---
+++ IaaS/xen/new.groovy
@@ -0,0 +1,1 @@
+disk = ['phy:/dev/xenv/windows,hda,w','file:/media/rfs/data/img/Win10_Pro_1511_English_x64_March_2016.iso,hdc:cdrom,r']
--- Xen/instal.groovy
+++
@@ -1,1 +0,0 @@
-disk = ['phy:/dev/xenv/windows,hda,w','file:/media/rfs/data/img/Win10_Pro_1511_English_x64_March_2016.iso,hdc:cdrom,r']
