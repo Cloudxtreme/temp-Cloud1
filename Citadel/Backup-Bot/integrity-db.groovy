91a1342f71522c7de829b39b8e75bffc8401fb89
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-10 05:21:56 -0500

    Create integrity-db.groovy

---
+++ Citadel/Backup-Bot/integrity-db.groovy
@@ -0,0 +1,7 @@
+Build a Checksum database using tools like Tripwire, OSSEC!
+
+https://en.wikipedia.org/wiki/Open_Source_Tripwire
+
+https://en.wikipedia.org/wiki/OSSEC
+
+https://en.wikipedia.org/wiki/File_integrity_monitoring
