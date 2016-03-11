ace734b255ffe84d4baedb4d3b455802039535e2
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-10 19:41:25 -0500

    Update audio.groovy

--- IaaS/Xen/audio.groovy
+++ IaaS/Xen/audio.groovy
@@ -5,4 +5,7 @@ https://forums.linuxmint.com/viewtopic.php?f=42&t=160527
 http://askubuntu.com/questions/557906/problem-starting-jack-server-jack-server-is-not-running-or-cannot-be-started
 
 # install
-sudo apt-get -y install vlc 
+sudo apt-get -y install jackd2 qjackctl alsaplayer alsaplayer-jack vlc 
+
+
+alsaplayer -o jack ~/Desktop/Allegro.mp3
