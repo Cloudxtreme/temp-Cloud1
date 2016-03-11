c79bcb9fa1057a1f58e9f2de1a4288bec9d712b3
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-10 19:37:35 -0500

    Update audio.groovy

--- IaaS/Xen/audio.groovy
+++ IaaS/Xen/audio.groovy
@@ -3,3 +3,6 @@ https://forums.linuxmint.com/viewtopic.php?f=42&t=160527
 
 ## Try
 http://askubuntu.com/questions/557906/problem-starting-jack-server-jack-server-is-not-running-or-cannot-be-started
+
+# install
+sudo apt-get -y install vlc 
