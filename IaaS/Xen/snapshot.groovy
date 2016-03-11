e414388ed7dc95ea1d3e92af579bc12ccc07b866
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-10 20:24:11 -0500

    Update snapshot.groovy

--- IaaS/Xen/snapshot.groovy
+++ IaaS/Xen/snapshot.groovy
@@ -5,3 +5,15 @@ size=15
 
 sudo lvcreate -s -n $lv$snap -L ${size}g $vg/$lv
 sudo lvs
+
+Snapshot(){
+  snap='-prejack' ; lv=windows ; vg=debian-vg ; size=15
+  sudo lvremove /dev/$vg/$lv$snap
+  sudo lvcreate -s -n $lv$snap -L ${size}g $vg/$lv
+}
+
+
+Restore(){
+  # https://www.google.com/search?q=lvm+restore+snapshot
+}
+
