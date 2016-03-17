0880bfbeab2577ca782ac00babd1e799478d0bfa
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-16 18:26:34 -0700

    Update install-ansible.sh

--- .Juju/Charms/master/install-ansible.sh
+++ .Juju/Charms/master/install-ansible.sh
@@ -1,3 +1,5 @@
 # Install   http://docs.ansible.com/ansible/intro_installation.html#latest-releases-via-pip
 apt-get install -y python-dev python-pip python-paramiko python-markupsafe
 pip install ansible
+
+pip install markupsafe  # Only required for Debian 8!
