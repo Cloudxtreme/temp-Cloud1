2c67b302d4fd28b0af10a463f7c8a7a68ba8b72b
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-16 18:54:56 -0700

    Update dev-samba.sh

--- .Juju/Charms/master/dev-samba.sh
+++ .Juju/Charms/master/dev-samba.sh
@@ -2,3 +2,14 @@
 #   https://wiki.debian.org/SambaServerSimple
 apt-get install samba -y
 
+
+sed -i 's/read only = yes/read only = no/' /etc/samba/smb.conf
+
+echo '''
+[Zen]
+   comment = Zen Cloud devel
+   read only = no
+   locking = no
+   path = /root/Zen
+   guest ok = yes
+''' | tee -a /etc/samba/smb.conf
