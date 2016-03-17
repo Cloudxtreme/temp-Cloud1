baa17f9297d4c63fbfda03e62c07cf23148832af
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-16 21:25:46 -0700

    Update install.sh

--- .Juju/Charms/master/tasks/install.sh
+++ .Juju/Charms/master/tasks/install.sh
@@ -1,5 +1,5 @@
 # Install   http://docs.ansible.com/ansible/intro_installation.html#latest-releases-via-pip
-apt-get install -y git openjfx python-dev python-pip python-paramiko python-markupsafe
+apt-get install -y default-jdk git openjfx python-dev python-pip python-paramiko python-markupsafe
 pip install ansible
 
 hostnamectl set-hostname master
