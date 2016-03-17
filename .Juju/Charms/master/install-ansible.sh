f846e6c78dd032b770a9fce5bdebb8fdc95c3b9d
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-16 18:23:26 -0700

    Update install-ansible.sh

--- .Juju/Charms/master/install-ansible.sh
+++ .Juju/Charms/master/install-ansible.sh
@@ -1,4 +1,3 @@
-
+# Install   http://docs.ansible.com/ansible/intro_installation.html#latest-releases-via-pip
 apt-get install -y python-dev python-pip python-paramiko python-markupsafe
-#pip install markupsafe
 pip install ansible
