9a0cd6072741c2a6c8f25d0f62476ab7e421afb7
Entrepreneur's Network Inc <bennt89@gmail.com>
2016-03-16 18:41:34 -0700

    Update and rename install-ansible.sh to install.sh

--- .Juju/Charms/master/install.sh
+++ .Juju/Charms/master/install.sh
@@ -1,5 +1,5 @@
 # Install   http://docs.ansible.com/ansible/intro_installation.html#latest-releases-via-pip
-apt-get install -y python-dev python-pip python-paramiko python-markupsafe
+apt-get install -y git python-dev python-pip python-paramiko python-markupsafe
 pip install ansible
 
 pip install markupsafe  # Only required for Debian 8!
